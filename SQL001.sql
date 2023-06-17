c7a141d11aed6bbf423b40382a3b482256245281
noobdataengineer <136634027+noobdataengineer@users.noreply.github.com>
2023-06-17 22:14:23 +0530

    #Integrity_Constraints

--- SQL001.sql
+++ SQL001.sql
@@ -20,7 +20,7 @@ SHOW TABLES;
 SELECT * FROM employees;
 
 --- Inserting values into the Tables;
-INSERT INTO employees VALUES(193321, 'Arpit', 'Saxena', 1500, 'IND', '2016-03-12');
+INSERT INTO employees VALUES(103321, 'Arpit', 'Saxena', 1500, 'IND', '2016-03-12');
 INSERT INTO employees VALUES(123321, 'Sudhir', 'Kumar', 1200, 'IND', '2020-03-08');
 INSERT INTO employees VALUES(183343, 'Alysaa', 'Girald', 5000, 'USA', '2015-07-21');
 INSERT INTO employees VALUES(183141, 'Justin', 'Wagner', 5100, 'USA', '2015-05-21');
@@ -32,6 +32,22 @@ INSERT INTO employees (ID, Firstname, Lastname, Salary) VALUES(163321, 'Abhay', 
 ALTER TABLE employees ADD SEX VARCHAR(7);
 ALTER TABLE employees ADD emailaddress VARCHAR(50);
 ALTER TABLE employees ADD CONSTRAINT emailaddress UNIQUE (emailaddress), ADD CONSTRAINT ID PRIMARY KEY (ID);
+--- INTEGRITY CONSTRAINT - check that date of joining must be less than current date
+ALTER TABLE employees ADD CONSTRAINT CHECK (DOJ < CURRENT_DATE);
+
+--- INTEGRITY CONSTRAINT
+CREATE TABLE IF NOT EXISTS projects(
+	ID INT PRIMARY KEY,
+    Project VARCHAR(40),
+    PROJECTCODE VARCHAR(3),
+    ProjectDate DATE DEFAULT '2024-01-01',
+    Duration INT,
+    Stipend DOUBLE,
+    skill VARCHAR(250),
+    UNIQUE (PROJECTCODE),
+    CHECK (Stipend > 500));
+
+
 
 --- ADD VALUES FOR NEW COLUMN IN EXISTING TABLE AS PER THE SEQUENCE;
 INSERT INTO employees (SEX) VALUES('Male'); --- WRONG CREATES A NEW RECORD
@@ -53,6 +69,41 @@ ALTER TABLE employees DROP emailaddress;
 --- deletioN OF CONSTRAINT
 ALTER TABLE employees DROP CONSTRAINT emailaddress;
 
---- RENAME OF ROW
+--- RENAME OF COLUMN
 ALTER TABLE employees RENAME COLUMN SEX TO Gender;
 
+--- CREATION OF NEW TABLE
+CREATE TABLE IF NOT EXISTS skill(
+	EID INT,
+    TECHNOLOGY VARCHAR(25));
+    
+SELECT * FROM skill ORDER BY EID;
+
+INSERT INTO skill VALUES(103321, 'Agiloft'), (103321, 'SQL'), (123321, 'SQL'), (123321, 'Agiloft'), (123321, 'Python'), (123321, 'AWS');
+INSERT INTO skill VALUES(183343, 'Python'), (183141, 'Python'), 
+	(183343, 'SQL'), (183141, 'SQL'), 
+    (183343, 'R'), (183141, 'MS-EXCEL'), 
+    (183343, 'Power BI'), (183141, 'Tableau');
+    
+--- See Table Defination
+SHOW CREATE TABLE skill;
+    
+--- RETURN THE NAME OF EMPLOYEES WITH ID HAVING PROFICIENCY IN Python, SQL, Power BI 
+/*
+SELECT ID, CONCAT(Firstname, ' ', Lastname) AS Fullname FROM employees;
+SELECT EID FROM skill GROUP BY EID;
+
+SELECT ID, CONCAT(Firstname, ' ', Lastname) AS Fullname FROM employees AS e
+	INNER JOIN skill AS s
+    ON s.EID=e.ID 
+    WHERE 
+	TECHNOLOGY='Python'; 
+*/
+    
+--- RETURN THE PAGES IDs OF FACEBOOK PAGES CONDITION WITHOUT ANY LIKES IN ASCENDING ORDER
+SELECT page_id FROM pages WHERE page_id NOT IN (SELECT DISTINCT(page_id) FROM pages_likes);
+
+--- 2ND WAY
+SELECT page_id FROM pages LEFT JOIN pages_likes AS p
+	ON pages.page_id=p.page_id WHERE
+    liked_date = NULL;
